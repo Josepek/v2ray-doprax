ce35a5e4192a9fee92159e40c764b3df7f2cf301
Yousef Keyghobadi <usef.keyghobadi@gmail.com>
2024-05-23 23:02:50 +0330

    change env file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM v2fly/v2fly-core
 
+ENV UUID=${UUID}
+ENV VMESS_WSPATH=${VMESS_WSPATH}
+ENV VLESS_WSPATH=${VLESS_WSPATH}
+
 COPY config.json /etc/v2ray/config.json
 EXPOSE 8080
 
--- init.sh
+++ init.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-# Initialize V2Ray
+# Ensure UUID is set
+if [ -z "$UUID" ]; then
+    echo "UUID is not set. Exiting."
+    exit 1
+fi
+
+# Initialize V2Ray configuration
 if [ ! -f /etc/v2ray/config.json ]; then
     cp /config.json /etc/v2ray/config.json
 fi
+
+# Replace placeholders in config.json with environment variables
+sed -i "s/UUID_PLACEHOLDER/$UUID/g" /etc/v2ray/config.json
+sed -i "s|VMESS_WSPATH_PLACEHOLDER|$VMESS_WSPATH|g" /etc/v2ray/config.json
+sed -i "s|VLESS_WSPATH_PLACEHOLDER|$VLESS_WSPATH|g" /etc/v2ray/config.json
 
 # Start Supervisor
 supervisord -c /supervisord.conf
